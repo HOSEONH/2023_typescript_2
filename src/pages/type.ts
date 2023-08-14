e0d89edd13eb10e3d7387022139b713a1b9a620c
황호선 <3262398@naver.com>
2023-08-14 12:29:59 +0900

    0804

--- src/pages/type.ts
+++ src/pages/type.ts
@@ -2,5 +2,5 @@ function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
-const result = addNumbers(4, 6);
-console.log(result); // 출력 결과 : 10
+const result = addNumbers(4, 2);
+console.log(result); // 출력 결과 : 6
