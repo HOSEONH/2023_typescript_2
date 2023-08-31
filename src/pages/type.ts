57c6b7481cc097e053008d5872aa18f10b8b8632
황호선 <3262398@naver.com>
2023-09-01 07:45:16 +0900

    0901

--- src/pages/type.ts
+++ src/pages/type.ts
@@ -1,7 +1,10 @@
-function greet(name?: string): string {
-    if (name) {
-      return `Hi, ${name}!`;
-    } else {
-      return `Hi!`;
-    }
+function displayType(value: string | number): void {
+  if (typeof value === "string") {
+    console.log("Type: string, Value:", value.toUpperCase());
+  } else {
+    console.log("Type: number, Value:", value.toFixed(2));
+  }
 }
+
+displayType("hello"); // 문자열 출력
+displayType(3.14159); // 숫자 출력
