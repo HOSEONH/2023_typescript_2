1ca82eedcf593197621e65b2253a93fb3171e6bb
황호선 <3262398@naver.com>
2023-09-05 16:28:05 +0900

    0905

--- src/pages/type.ts
+++ src/pages/type.ts
@@ -1,10 +1,34 @@
-function displayType(value: string | number): void {
-  if (typeof value === "string") {
-    console.log("Type: string: ");
-  } else {
-    console.log("Type: number: ");
+interface StringValidator {
+  isAcceptable(s: string): boolean;
+}
+
+let lettersRegexp = /^[A-Za-z]+$/;
+let numberRegexp = /^[0-9]+$/;
+
+class LettersOnlyValidator implements StringValidator {
+  isAcceptable(s: string) {
+    return lettersRegexp.test(s);
   }
 }
 
-displayType("hello"); 
-displayType(3.14159); 
+class ZipCodeValidator implements StringValidator {
+  isAcceptable(s: string) {
+    return s.length === 5 && numberRegexp.test(s);
+  }
+}
+
+// 시도해 볼 샘플
+let strings = ["Hello", "98052", "101"];
+
+// 사용할 검사기
+let validators: { [s: string]: StringValidator } = {};
+validators["ZIP code"] = new ZipCodeValidator();
+validators["Letters only"] = new LettersOnlyValidator();
+
+// 각 문자열이 각 검사기를 통과했는지 표시
+for (let s of strings) {
+  for (let name in validators) {
+    let isMatch = validators[name].isAcceptable(s);
+    console.log(`'${s}' ${isMatch ? "matches" : "does not match"} '${name}'.`);
+  }
+}
