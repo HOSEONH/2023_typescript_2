ad97ed58e16fb868506be5aff857379004b3f98a
황호선 <3262398@naver.com>
2024-01-25 13:28:46 +0900

    0125

--- src/pages/type.ts
+++ src/pages/type.ts
@@ -17,7 +17,7 @@ class ZipCodeValidator implements StringValidator {
   }
 }
 
-let strings = ["Hello", "9782211", "10132"];
+let strings = ["Hello", "978211", "10132"];
 
 // 사용할 검사기
 let validators: { [s: string]: StringValidator } = {};
